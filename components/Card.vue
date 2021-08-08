c2023471eff0ee69a80d36ea15748b4028f432df
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-08 13:56:53 -0300

    create card component

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <img :src="post.feature_image" :alt="post.title" />
+    <div class="content">
+      <span v-text="post.authors[0].name" />
+      <p v-text="post.excerpt" />
+      <NuxtLink :to="{ path: post.slug }" v-text="post.title" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,7 @@
       <h2>Latest thoughts</h2>
       <ul>
         <li v-for="post in posts" :key="post.id">
-          <img :src="post.feature_image" :alt="post.title" />
-          <div class="content">
-            <span v-text="post.authors[0].name" />
-            <p v-text="post.excerpt" />
-            <NuxtLink :to="{ path: post.slug }" v-text="post.title" />
-          </div>
+            <Card :post="post" />
         </li>
       </ul>
     </main>
