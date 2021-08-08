602e4c5ee3abd3b377af8059101a5f038a5718f2
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-08 11:50:53 -0300

    update

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+
+export default {
+  target: "static",
+  ssr: false,
+  head: {
+    title: 'Blog - Gabriel Caiana',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Bem vindos ao meu blog!' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap',
+      },
+    ],
+  },
+
+  css: ['@/scss/main.scss'],
+
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+    css: false,
+
+    beforeEnter(el) {
+      this.$gsap.set(el, {
+        opacity: 0
+      })
+    },
+
+    enter(el, done) {
+      this.$gsap.to(el, {
+        opacity: 1,
+        duration: 1,
+        ease: 'power3.out',
+        onComplete: done
+      })
+    },
+
+    leave(el, done) {
+      this.$gsap.to(el, {
+        opacity: 0,
+        duration: 1,
+        ease: 'power3.out',
+        x: 500,
+        onComplete: done
+      })
+    }
+  },
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    'nuxt-gsap-module'
+  ],
+
+  styleResources: {
+    scss: []
+   },
+
+  modules: ['@nuxtjs/style-resources'],
+
+  build: {
+    extractCSS: true,
+    babel:{
+      plugins: [
+        ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+      ]
+    }
+  }
+}
