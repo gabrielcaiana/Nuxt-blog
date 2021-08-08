0c4d55c6aa3e3a1bf7896c60cedc6bc973958637
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-08 12:31:12 -0300

    create sass mixins

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="navbar">
+        <nuxt-link to="/">Gabriel Caiana</nuxt-link>
+        <nav>
+          <ul>
+            <li><nuxt-link to="/who">Who?</nuxt-link></li>
+            <li><nuxt-link to="/posts">Posts</nuxt-link></li>
+            <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="default">
+    <Header />
   <nuxt />
   </div>
 </template>
